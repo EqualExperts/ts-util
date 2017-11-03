0ea18d696f229daaeb6eaf6d77b96cb465f9c508
Carlos Fraga <cfraga@equalexperts.com>
2017-11-03 11:39:39 +0000

    #203 : Exports CollectionBuilder type in MongoDB Adapter
    @psousa50
    @cfraga

--- lib/mongodb/Adapter.d.ts
+++ lib/mongodb/Adapter.d.ts
@@ -1,6 +1,8 @@
 import * as mongo from "mongodb";
+import { Collection } from "mongodb";
 export interface IdDto {
     _id: string;
 }
+export declare type CollectionBuilder<T> = (collectionName: string) => Collection<T>;
 export declare const buildCollection: <T>(db: mongo.Db) => (collectionName: string) => mongo.Collection<T>;
 export declare const buildGetCollectionMongoAdapter: <T>(collection: mongo.Collection<T>) => () => Promise<T[]>;

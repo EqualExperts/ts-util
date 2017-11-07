8f6977939b0406a759ce634dca19f694dd3c3aaf
Carlos Fraga <cfraga@equalexperts.com>
2017-11-07 16:12:26 +0000

    #203 : removed lib/folder from the repo
    @leenabora
    @cfraga

--- lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-import * as MongoAdapter from "./mongodb/adapter";
-export { MongoAdapter };
--- lib/mongodb/Adapter.d.ts
+++
@@ -1,8 +0,0 @@
-import * as mongo from "mongodb";
-import { Collection } from "mongodb";
-export interface IdDto {
-    _id: string;
-}
-export declare type CollectionFactory<T> = (collectionName: string) => Collection<T>;
-export declare const buildCollectionFactory: <T>(db: mongo.Db) => (collectionName: string) => mongo.Collection<T>;
-export declare const buildGetCollectionMongoAdapter: <T>(collection: mongo.Collection<T>) => () => Promise<T[]>;

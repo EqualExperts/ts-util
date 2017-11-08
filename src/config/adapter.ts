14fd5caf07be31ddd4c55715e78c8c574eb0b7fc
Pedro Sousa <psousa@equalexperts.com>
2017-11-08 11:25:20 +0000

    #199 : Adds support to validate env vars
    @rafaelfiume
    @psousa50

--- src/config/adapter.ts
+++ src/config/adapter.ts
@@ -1,5 +1,7 @@
 import convict = require("convict")
 import { Schema, SchemaObj } from "convict"
+import "fp-ts"
+import { Either, right, left } from "fp-ts/lib/Either"
 
 export type Definition = {
     [name: string]: Format,
@@ -9,17 +11,33 @@ export type Format = {
     format?: string | any[],
 }
 
-export const getValueForEnvVar = (key: string): string => (
-    process.env[key] as string
-)
-
 type EnvironmentVariables = (key: string) => string
-export const buildConfigAdapter = (definition: Definition): EnvironmentVariables => (key: string) => {
+export const buildConfigAdapter = (definition: Definition): Either<string, EnvironmentVariables> => {
     const schema: Schema = {}
     Object.keys(definition).forEach((name) =>
-        schema[name] = { env: name, format: definition[name].format, default: null },
+        schema[name] = {
+            env: name,
+            format: definition[name].format ? definition[name].format : "*",
+            default: null,
+        },
     )
 
     const config = convict(schema)
-    return config.get(key)
+
+    const missingVarMessages: string = Object.keys(definition).reduce((errorMessages, name) => {
+        const value = config.get(name)
+        return value == null ? errorMessages + name + ": defined but not set;" : errorMessages
+    }, "")
+
+    if (missingVarMessages.length > 0) {
+        return left(missingVarMessages)
+    }
+
+    try {
+        config.validate()
+    } catch (e) {
+        return left(e.message)
+    }
+
+    return right((key: string) => config.get(key))
 }
--- test/config/adapter.spec.ts
+++ test/config/adapter.spec.ts
@@ -1,35 +1,103 @@
 import "jest"
-import { buildConfigAdapter, getValueForEnvVar } from "../../src/config/adapter"
+import "fp-ts"
+import { buildConfigAdapter } from "../../src/config/adapter"
+import { Either } from "fp-ts/lib/Either"
 
 describe("ConfigAdapter", () => {
     it("reads an environment variable", () => {
-        const key = "FRUIT_ENV"
+        const name = "FRUIT_ENV"
         const value = "oranges"
-        givenThereIsAnEnvironmentVar(key, value)
+        givenThereIsAnEnvironmentVar(name, value)
         const definition = {
             FRUIT_ENV: {},
         }
 
         const underTest = buildConfigAdapter(definition)
-        const result = underTest(key)
+        const result = underTest.fold(
+            (left) => fail("expected to have a right with configAdapter: got left instead"),
+            (envVars) => envVars(name),
+        )
 
         expect(result).toBe(value)
     })
 
     it("can deal with multiple environment variables", () => {
         givenThereIsAnEnvironmentVar("FRUIT_ENV", "oranges")
-        const key = "BOOKS_ENV"
+        const name = "BOOKS_ENV"
         const value = "To Whom the Bell Tols"
-        givenThereIsAnEnvironmentVar(key, value)
+        givenThereIsAnEnvironmentVar(name, value)
         const definition = {
             BOOKS_ENV: {},
             FRUIT_ENV: {},
         }
 
         const underTest = buildConfigAdapter(definition)
-        const result = underTest(key)
+        const result = underTest.fold(
+            (left) => fail("expected to have a right with configAdapter: got left instead"),
+            (envVars) => envVars(name),
+        )
 
         expect(result).toBe(value)
+    })
+
+    it("validates the format of the variable value", () => {
+        const name = "SOME_URL"
+        const value = "http://mondocao.com"
+        givenThereIsAnEnvironmentVar(name, value)
+        const definition = {
+            SOME_URL: {
+                format: "url",
+            },
+        }
+
+        const underTest = buildConfigAdapter(definition)
+        const result = underTest.fold(
+            (left) => fail("expected to have a right with configAdapter: got left instead"),
+            (envVars) => envVars(name),
+        )
+
+        expect(result).toBe(value)
+    })
+
+    it("returns an error when reading an env var with an incorrect format", () => {
+        const name = "SOME_URL"
+        const value = "_not_a_url"
+        givenThereIsAnEnvironmentVar(name, value)
+
+        const definition = {
+            SOME_URL: {
+                format: "url",
+            },
+        }
+
+        const maybeEnvVars = buildConfigAdapter(definition)
+        const result = maybeEnvVars.fold(
+            (errorMessage) => errorMessage,
+            (envVars) => "expected to have a left: got right instead",
+        )
+
+        expect(result).toBe(name + ": must be a URL: value was \"" + value + "\"")
+    })
+
+    it("returns an error when environment variable is defined but not set", () => {
+        const unexistentVar = "SOME_UNDEFINED_VAR"
+        const anotherUnexistentVar = "ANOTHER_UNDEFINED_VAR"
+
+        const definition = {
+            [unexistentVar]: {},
+            [anotherUnexistentVar]: {},
+        }
+
+        const maybeEnvVars = buildConfigAdapter(definition)
+        const result = maybeEnvVars.fold(
+            (errorMessage) => errorMessage,
+            (envVars) => "expected to have a left: got right instead",
+        )
+
+        expect(result).toBe(
+            unexistentVar + ": defined but not set" + ";" +
+            anotherUnexistentVar + ": defined but not set" + ";",
+        )
     })
 })
 

34700d613edd95b5540f97e9a2edbb738e948b7b
Carlos Fraga <cfraga@equalexperts.com>
2017-11-21 17:06:48 +0000

    #127 : Extracts TimeEntry adapter transformer and adds a test to it
    @leenabora
    @cfraga

--- src/10kfeet/timeEntryAdapter.ts
+++ src/10kfeet/timeEntryAdapter.ts
@@ -2,10 +2,17 @@ import * as fetch from "isomorphic-fetch"
 
 export type TimeEntryDto = {
     hours: number
-    email: string
     day: Date
     userId: number,
 }
+
+export const transform: (json: any) => TimeEntryDto[] = (json: any) =>
+        (json.data.map(
+            (data: any) => ({
+                hours: data.hours,
+                day: new Date(data.date),
+                userId: data.user_id,
+            } as TimeEntryDto)))
 
 export type FetchTimeEntryAdapter = (from: string, to: string) => Promise<TimeEntryDto[]>
 
@@ -15,17 +22,7 @@ export const buildFetchTimeEntryAdapter: (apiUrl: string, token: string) => Fetc
             return fetch(`${apiUrl}/time_entries?from=${from}&to=${to}`, {
                 headers: new Headers({ "content-type": "application/json", "auth": token }),
             }).then((response: Response) => (
-                response.json().then(
-                    (json: any) =>
-                        (json.data.map(
-                            (data: any) => ({
-                                hours: data.hours,
-                                email: "not_yet",
-                                day: new Date(data.date),
-                                userId: data.user_id,
-                            } as TimeEntryDto),
-
-                        )))
+                response.json().then(transform)
             ),
             )
         }
--- test/10kfeet/timeEntryAdapter.spec.ts
+++ test/10kfeet/timeEntryAdapter.spec.ts
@@ -1,5 +1,7 @@
 import "jest"
-import {buildFetchTimeEntryAdapter, TimeEntryDto} from "../../src/10kfeet/timeEntryAdapter"
+import * as fs from "fs"
+import * as path from "path"
+import { transform, buildFetchTimeEntryAdapter, TimeEntryDto} from "../../src/10kfeet/timeEntryAdapter"
 
 describe("10K Feet Time Entries", () => {
    // This test is ignored because we don't have test creds for 10kFt
@@ -19,3 +21,18 @@ describe("10K Feet Time Entries", () => {
         expect(result.length).toBe(4)
    } )
 } )
+
+describe(" TimeEntries from 10KFeet  ", () => {
+    test(" should transform into required fields", () => {
+        const tenKResponse: string =
+            fs.readFileSync(path.resolve(__dirname, "../stubs/10kfttimeentry_response.json"), "utf-8")
+
+        const tenKJSON = JSON.parse(tenKResponse)
+
+        const dto: TimeEntryDto = transform(tenKJSON)[0]
+
+        expect(dto.day).toEqual(new Date("2017-11-07"))
+        expect(dto.hours).toBe(1)
+        expect(dto.userId).toBe(380187)
+    })
+})

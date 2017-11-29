9ca5c9e976ddea03350da2f00436d07ce6d44ed5
Rafael Fiume <rfiume@equalexperts.com>
2017-11-29 09:13:56 +0000

    #142 - Defines contract (type) for csvExport function
    @rafaelfiume
    @leenabora

--- src/filesystem/csv.ts
+++ src/filesystem/csv.ts
@@ -1,10 +1,14 @@
 import { appendFile, appendFileSync } from "fs"
 import * as nano from "nano-seconds"
 
+export type FilePath = string
+export type CsvEntry = string
+export type ExportCsv = <T>(content: T[], transformer: (item: T) => CsvEntry) => Promise<FilePath>
+
 // TODO - (very very corner case scenario. as we are using a nano-seconds as a csv file prefix)
 // since csv file is generated using current nano timestamp, if we access this from multiple machines,
 // at the same time. It will append the content to the same file. We need to solve this if it is an issue.
-export const exportCsv = async <T>(content: T[], transformer: (item: T) => string): Promise<string> => {
+export const exportCsv: ExportCsv = async <T>(content: T[], transformer: (item: T) => CsvEntry): Promise<FilePath> => {
     const csvFileName = "/tmp/csv-" + nano.toISOString(nano.now()) + ".csv"
     content.forEach((c) => {
         const item = `${transformer(c)}\n`

16879c8d9135af25afa9cad4742d355a95083fe3
João Pinho <jpinho@equalexperts.com>
2018-03-06 11:17:18 +0000

    #303 - Consolidates work on GDriveGetFilesInFolderAdapter
    @jpinho

--- src/gsuite/driveAdapter.ts
+++ src/gsuite/driveAdapter.ts
@@ -7,9 +7,14 @@ import * as path from "path"
 export type GetGDriveFilesInFolderAdapter = (targetFolderId: string) => Promise<string[]>
 export type BuildGetGDriveFilesInFolderAdapter = (gSuiteClient: any) => GetGDriveFilesInFolderAdapter
 export type ReadGDriveFileAsyncHandler = (gdriveClient: any, fileId: string, fileName: string) => Promise<string>
+export type GDriveFileMetaInfo = { id: string, name: string }
+
+export const GDRIVE_SCOPES: string[] = ["https://www.googleapis.com/auth/drive.readonly"]
+export const GDRIVE_VERSION = "v3"
+export const GDRIVE_FINDFILESINFOLDER_PAGELIMIT = 100
 
 export const buildGetGDriveFilesInFolderAdapter: BuildGetGDriveFilesInFolderAdapter = (gSuiteConfig: GSuiteConfig) => {
-    const gsuiteClient = buildGSuiteClient(gSuiteConfig, ["https://www.googleapis.com/auth/drive.readonly"])
+    const gsuiteClient = buildGSuiteClient(gSuiteConfig, GDRIVE_SCOPES)
     return async (targetFolderId) => {
         await authorize(gsuiteClient)
         return getGDriveFilesInFolder(gsuiteClient, targetFolderId)
@@ -17,68 +22,45 @@ export const buildGetGDriveFilesInFolderAdapter: BuildGetGDriveFilesInFolderAdap
 }
 
 export const getGDriveFilesInFolder = async (gSuiteClient: any, targetFolderId: string) => {
-        const gdrive = google.drive({
-            version: "v3",
-            auth: gSuiteClient
-        })
-
-        const maybeFilesContainer: Promise<Array<Promise<string>>> =
-            new Promise<Array<Promise<string>>>((resolve, reject) => {
-                gdrive.files.list({ pageSize: 20, q: `parents in '${targetFolderId}'` }, (err: any, response: any) => {
-                    if (err) {
-                        console.error("GDrive files.list API returned an error: " + err)
-                        reject(err)
-                    }
-
-                    const files = response.data.files
-                    if (!files || files.length === 0) {
-                        console.log("No files found in folder")
-                        resolve([])
-                    }
-
-                    const maybeFiles: Array<Promise<string>> = []
-                    for (const file of files) {
-                        console.log("Found file", file.id, " | ", file.name)
-                        maybeFiles.push(readGDriveFileAsync(gdrive, file.id, file.name))
-                    }
-
-                    resolve(maybeFiles)
-                })
+    const gdrive = google.drive({
+        version: GDRIVE_VERSION,
+        auth: gSuiteClient
+    })
+    const getMaybeFiles = await new Promise<Array<Promise<string>>>((resolve, reject) => {
+        gdrive.files.list(
+            { pageSize: GDRIVE_FINDFILESINFOLDER_PAGELIMIT, q: `parents in '${targetFolderId}'` },
+            (err: any, response: any) => {
+                if (err) {
+                    reject(err)
+                }
+                const files = response.data.files
+                if (!files || files.length === 0) {
+                    resolve([])
+                }
+                const maybeFiles: Array<Promise<string>> =
+                    files.map((f: GDriveFileMetaInfo) => readGDriveFileAsync(gdrive, f.id, f.name))
+                resolve(maybeFiles)
             })
-
-        const getMaybeFiles = await maybeFilesContainer
-        return Promise.all(getMaybeFiles)
-    }
+    })
+    return Promise.all(getMaybeFiles)
+}
 
 export const readGDriveFileAsync: ReadGDriveFileAsyncHandler = (gdriveClient, fileId, fileName) => {
-        return new Promise<string>((resolve, reject) => {
-            const fileGet = gdriveClient.files.get({
-                fileId: `${fileId}`,
-                alt: "media"
-            }, {
-                    responseType: "stream"
-                }, (fgetError: any, fgetResp: any) => {
-                    if (fgetError) {
-                        console.error(fgetError)
-                        reject(fgetError)
-                    }
-                    const relativePath = path.join(os.tmpdir(), fileName)
-                    const dest = fs.createWriteStream(relativePath)
-                        .on("finish", () => {
-                            console.log("Downloaded %s!", fileName)
-                            resolve(path.resolve(relativePath))
-                        })
-                        .on("error", (errWritting) => {
-                            console.error("Error writing file!")
-                            reject(errWritting)
-                        })
-
-                    fgetResp.data
-                        .on("error", (streamErr: any) => {
-                            console.error("Error downloading file.")
-                            reject(streamErr)
-                        })
-                        .pipe(dest)
-                })
-        })
-    }
+    return new Promise<string>((resolve, reject) => {
+        const fileGet = gdriveClient.files.get(
+            { fileId: `${fileId}`, alt: "media" },
+            { responseType: "stream" },
+            (fgetError: any, fgetResp: any) => {
+                if (fgetError) {
+                    reject(fgetError)
+                }
+                const relativePath = path.join(os.tmpdir(), fileName)
+                const dest = fs.createWriteStream(relativePath)
+                    .on("finish", () => resolve(path.resolve(relativePath)))
+                    .on("error", reject)
+                fgetResp.data
+                    .on("error", reject)
+                    .pipe(dest)
+            })
+    })
+}
